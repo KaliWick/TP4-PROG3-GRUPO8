2071ff3bc3cc6905be56d0af4d61201ee153ed05
Jano-Guerra11 <janoguerra04@gmail.com>
2024-09-10 15:35:26 -0300

    aporte 1 ej 1

--- TP4-GRUPO8/Ejercicio1.aspx.cs
+++ TP4-GRUPO8/Ejercicio1.aspx.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
+using System.Data;
 namespace TP4_GRUPO8
 {
     public partial class Ejercicio1 : System.Web.UI.Page
     {
+        string rutaBD = "Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=False";
+        string consultaProvincias = "select * from provincias";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                SqlConnection cn = new SqlConnection(rutaBD);
+                cn.Open();
+                SqlDataAdapter adaptador = new SqlDataAdapter(consultaProvincias, cn);
+                DataSet ds = new DataSet();
+                adaptador.Fill(ds,"Provincias");
+            }
         }
     }
 }
