45e7c963fe795f791b69a2a893521ad8cd157c01
Jano-Guerra11 <janoguerra04@gmail.com>
2024-09-11 16:40:59 -0300

    aporte 1 ej 3

--- TP4-GRUPO8/Ejercicio3a.aspx.cs
+++ TP4-GRUPO8/Ejercicio3a.aspx.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
 namespace TP4_GRUPO8
 {
     public partial class Ejercicio3 : System.Web.UI.Page
@@ -13,12 +13,18 @@ namespace TP4_GRUPO8
         string rutaBD = "Data Source=localhost\\sqlexpress;Initial Catalog=Libreria;Integrated Security=True";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+
             //CONEXION CON SQL
             SqlConnection cn = new SqlConnection(rutaBD);
             cn.Open();
-
+            SqlCommand cmdTemas = new SqlCommand("select * from Temas", cn);
+            SqlDataReader dr = cmdTemas.ExecuteReader();
+            
 
             cn.Close();
+            }
         }
 
         protected void lnkbtnVerLibros_Click(object sender, EventArgs e)
